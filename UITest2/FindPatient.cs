7246e3d46490bb4e3cda727d7c1693426fc3360f
Sabrowna <60734481+Sabrowna@users.noreply.github.com>
2021-03-15 16:43:40 +0100

    MPC solution

---
+++ UITest2/FindPatient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest2
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class FindPatient
+    {
+        IApp app;
+        Platform platform;
+
+        public FindPatient(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+            app.Tap("PrivateUserLoginButton");
+            app.Tap("FindPatientTab");
+
+        }
+
+        [Test]
+        public void Entering1234567890AsSocSec_PressingFindPatient_NameSetToJohnDoe()
+        {
+            //Arrange
+            app.Tap("SocSecSearch");
+            app.EnterText("SocSecSearch", "1234567890");
+
+            //Act
+            app.Tap("FindPatientButton");
+            var socSec = app.Query("SocSearch").Single().ToString();
+
+            //Assert
+            Assert.That(socSec, Is.EqualTo("1234567890"));
+        }
+    }
+}
